4e572e61e9b3198daf3bc971004c56f5ccd5d77f
unknown <707373265@qq.com>
2022-03-14 17:41:22 +0800

    new feature:wallet connect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
-import NavBar from "./components/NavBar";
-import { Switch, Route } from "react-router-dom";
 import Home from './components/Home';
-
+import { DAppProvider, Mainnet } from '@usedapp/core'
+const config = {
+  readOnlyChain: Mainnet.chainId,
+  readOnlyUrls: {
+    [Mainnet.chainId]: 'https://mainnet.infura.io/v3/62687d1a985d4508b2b7a24827551934',
+  },
+}
 
 const App = () => {
   return (
+  <DAppProvider config={config}>
     <Home/>
+  </DAppProvider>
   );
 };
 
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,7 +7,47 @@ import Roadmap from '../Roadmap'
 import FAQ from '../FAQ'
 import Team from '../Team'
 import Footer from '../Footer'
+import { useEthers, shortenAddress, useLookupAddress } from '@usedapp/core'
+import { message } from 'antd'
+import Web3Modal from 'web3modal'
+import WalletConnectProvider from '@walletconnect/web3-provider'
+import React, { useEffect, useState } from 'react'
 const Home = () => {
+    const { account, activate, deactivate } = useEthers()
+    const ens = useLookupAddress()
+    const { error } = useEthers()
+    useEffect(() => {
+      if (error) {
+        message.error(error.message)
+      }
+    }, [error])
+    const activateProvider = async () => {
+      const providerOptions = {
+        injected: {
+          display: {
+            name: 'Metamask',
+            description: 'Connect with the metamask in your Browser',
+          },
+          package: null,
+        },
+        walletconnect: {
+          package: WalletConnectProvider,
+          options: {
+            bridge: 'https://bridge.walletconnect.org',
+            infuraId: '57fc2c19095745e59ab96a4aa87dada8',
+          },
+        },
+      }
+      const web3Modal = new Web3Modal({
+          providerOptions,
+        })
+        try {
+          const provider = await web3Modal.connect()
+          await activate(provider)
+        } catch (error) {
+          message.error(error.message)
+        }
+      }
     return (
       <div className="container">
         <NavBar />
@@ -17,7 +57,7 @@ const Home = () => {
             <img src={iExpress} className="express-img"></img>
           </div>
           <div className="coming-button">
-            <button className="new-button" > coming soon </button>
+            <button className="new-button" onClick={activateProvider}> {account? ens ?? shortenAddress(account):"Connect Wallet"} </button>
           </div>
           <div className="intro">
             <p>
